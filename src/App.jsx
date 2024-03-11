c591df7e348ce398aafd1cd1464b008e2337e737
Vy Mai <aidentmai@gmail.com>
2024-03-11 03:06:24 -0700

    Finished homepage and added React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import HomePage from "./routes/homePage/homePage";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 function App() {
-  return (
-    <div>Hello World</div>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <HomePage /> },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,38 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Search for your dream place today by location or price! You can also
+            search by Buy or Rent tab depending on your circumstances.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>20+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
